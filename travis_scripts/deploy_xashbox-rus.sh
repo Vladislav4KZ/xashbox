6e542fc4cd989c34f8aea090ff4ec5e959c29197
Владислав Сухов <22411953+Vladislav4KZ@users.noreply.github.com>
2021-03-03 21:07:03 +0600

    Update deploy_xashbox-rus.sh

--- travis_scripts/deploy_xashbox-rus.sh
+++ travis_scripts/deploy_xashbox-rus.sh
@@ -1,20 +1,17 @@
 rm -rf touch userconfig.d xashbox
-cd $BUILD_HOME
 echo CLONING REPOSITORY
 git clone -b xashbox-rus https://github.com/$GIT_NAME/xashbox xashbox-rus
 echo CHECKING AVAILABILITY
 ls -a
 cd xashbox-rus
-export BUILD_DIR=$PWD
-echo !!! CHECKING VARIABLE !!!
-ls $BUILD_HOME
 sh $SCRIPT_DIR/encode_to_cp1251.sh
 sh $SCRIPT_DIR/cleanup.sh
 export XASHBOX_NAME=xashbox-rus
+export XASHBOX_DIR=$BUILD_DIR/$XASHBOX_NAME
 cd $SCRIPT_DIR
 sh makepak.sh
 sh makezip.sh
 cd $BUILD_DIR
 mv $XASHBOX_NAME.zip $BUILD_HOME/$XASHBOX_NAME.zip
 mv $XASHBOX_NAME.pak $BUILD_HOME/$XASHBOX_NAME.pak
-cd $BUILD_HOME
+cd $BUILD_H
