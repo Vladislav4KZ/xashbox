d7991e9aacdedf60338b7743a6bd69f92ee2bfd6
Владислав Сухов <22411953+Vladislav4KZ@users.noreply.github.com>
2021-03-03 21:15:53 +0600

    Update makepak.sh

--- travis_scripts/makepak.sh
+++ travis_scripts/makepak.sh
@@ -4,4 +4,4 @@ cd $SCRIPT_DIR
 curl https://raw.githubusercontent.com/FWGS/xash3d-fwgs/master/scripts/makepak.py > makepak.py
 cd $XASHBOX_DIR && mkdir makepak
 cp -r -t makepak/ touch userconfig.d xashbox
-python3 $SCRIPT_DIR/makepak.py $BUILD_DIR/makepak/ $XASHBOX_NAME.pak
+python3 $SCRIPT_DIR/makepak.py $XASHBOX_DIR/makepak/ $XASHBOX_NAME.pak
